12f5ae678360749cf5dfca6d583efb72ce2298cd
Hossein Noor Ramsis <hosseinnoorramsis5@gmail.com>
2024-06-01 20:26:44 +0330

    feat: add the dark them js

--- src/JavaScript/script.js
+++ src/JavaScript/script.js
@@ -3,6 +3,11 @@ const menu = document.getElementById("menu");
 const main = document.getElementById("main");
 const items = document.getElementsByClassName("item");
 const nav = document.getElementById("nav");
+const backUp = document.getElementById("back-up");
+const themBtn = document.getElementById("them-btn");
+const home = document.getElementById("home");
+const homeLink = document.getElementById("home-link");
+const footer = document.getElementById("footer");
 
 function menuFunction() {
   if (menu.className === "menu") {
@@ -17,6 +22,7 @@ function hideMenu() {
   menu.className = "menu";
 }
 main.onclick = hideMenu;
+footer.onclick = hideMenu;
 for (let i = 0; i < items.length; i++) {
   items[i].onclick = hideMenu;
 }
@@ -30,5 +36,27 @@ function addNavShadow() {
 }
 window.addEventListener("scroll", addNavShadow)
 
-// them js function
+function addBackUpBtn() {
+  if (window.scrollY > 1000) {
+    backUp.style.display = "block";
+  } else {
+    backUp.style.display = "none";
+  }
+}
+window.addEventListener("scroll", addBackUpBtn)
 
+home.addEventListener("scroll", function() {
+  if (window.scrollY > 0) {
+    homeLink.classList.add("show")
+  }
+})
+// them js function
+function changeTheme() {
+  console.log(8)
+  if (document.body.classList.contains("dark")) {
+    document.body.classList.remove("dark");
+  } else {
+    document.body.classList.add("dark");
+  }
+}
+themBtn.addEventListener("click", changeTheme)
